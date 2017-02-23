53b1c7c0994933e635521159ef4af64c749d89f7
Benjamin Kastelic <kastelic.benjamin@gmail.com>
2017-02-23 18:33:55 +0100

    updated publish tool

--- PublishToAwsS3/Program.cs
+++ PublishToAwsS3/Program.cs
@@ -4,36 +4,45 @@ using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 using Amazon.S3.Util;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PublishToAwsS3
 {
     class Program
     {
+        private static string bucketName;
+        private static string sourceDirectory;
+
         static void Main(string[] args)
         {
-            CheckBucket("ssv-stoparica");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter required arguments. Usage: PublishToAwsS3 <bucketName> <sourceDirectory>");
+                return;
+            }
+
+            bucketName = args[0];
+            sourceDirectory = args[1];
+
+            CheckBucket();
         }
 
-        private static void CheckBucket(string bucketName)
+        private static void CheckBucket()
         {
             using (var client = new AmazonS3Client(Amazon.RegionEndpoint.EUCentral1))
             {
                 if (!(AmazonS3Util.DoesS3BucketExist(client, bucketName)))
                 {
-                    CreateBucket(bucketName, client);
+                    CreateBucket(client);
                 }
 
                 // Upload files.
-                UploadFiles("F:\\Development\\FireDeptStopwatch\\FireDeptStopwatch\\deploy", bucketName, client);
+                UploadFiles(client);
+                //UploadFiles("F:\\Development\\FireDeptStopwatch\\FireDeptStopwatch\\deploy", bucketName, client);
             }
         }
 
-        private static void CreateBucket(string bucketName, AmazonS3Client client)
+        private static void CreateBucket(AmazonS3Client client)
         {
             try
             {
@@ -61,29 +70,29 @@ namespace PublishToAwsS3
             }
         }
 
-        private static void UploadFiles(string directory, string bucketname, IAmazonS3 client)
+        private static void UploadFiles(IAmazonS3 client)
         {
             try
             {
                 TransferUtility transferUtility = new TransferUtility(client);
-                var filePaths = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+                var filePaths = Directory.GetFiles(sourceDirectory, "*", SearchOption.AllDirectories);
 
                 foreach (var filePath in filePaths)
                 {
                     var lastWrite = File.GetLastWriteTime(filePath).ToUniversalTime();
-                    var key = filePath.Replace(directory, @"");
+                    var key = filePath.Replace(sourceDirectory, @"");
                     if (key.StartsWith(@"\"))
                         key = key.Substring(1);
                     key = key.Replace(@"\", @"/");
 
                     bool fileExists = false, fileNewer = false;
 
-                    var fileInfo = new S3FileInfo(client, bucketname, key);
+                    var fileInfo = new S3FileInfo(client, bucketName, key);
                     if ((fileExists = fileInfo.Exists))
                     {
                         var getObjectMetadataRequest = new GetObjectMetadataRequest
                         {
-                            BucketName = bucketname,
+                            BucketName = bucketName,
                             Key = key
                         };
 
@@ -96,7 +105,7 @@ namespace PublishToAwsS3
                     {
                          var uploadRequest = new TransferUtilityUploadRequest
                         {
-                            BucketName = bucketname,
+                            BucketName = bucketName,
                             FilePath = filePath,
                             Key = key,
                             CannedACL = S3CannedACL.PublicRead
@@ -108,21 +117,6 @@ namespace PublishToAwsS3
                         transferUtility.Upload(uploadRequest);
                     }
                 }
-
-                //TransferUtility transferUtility = new TransferUtility(client);
-
-                //TransferUtilityUploadDirectoryRequest uploadDirectoryRequest = new TransferUtilityUploadDirectoryRequest
-                //{
-                //    BucketName = bucketname,
-                //    Directory = "D:\\Users\\Benjamin\\Desktop\\deploy",
-                //    SearchOption = SearchOption.AllDirectories,
-                //    CannedACL = S3CannedACL.PublicRead
-                //};
-
-                //uploadDirectoryRequest.UploadDirectoryProgressEvent +=
-                //    new EventHandler<UploadDirectoryProgressArgs>(UploadRequest_UploadPartProgressEvent);
-
-                //transferUtility.UploadDirectory(uploadDirectoryRequest);
             }
             catch (AmazonS3Exception amazonS3Exception)
             {
