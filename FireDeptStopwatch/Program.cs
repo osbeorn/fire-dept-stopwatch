869a9ba46af93049c03c389cfa8ff49794a17ea3
Benjamin Kastelic <kastelic.benjamin@gmail.com>
2018-02-13 07:12:33 +0100

    prevent multiple instances of the application

--- FireDeptStopwatch/Program.cs
+++ FireDeptStopwatch/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,15 +10,25 @@ namespace FireDeptStopwatch
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "{2963d2b2-e086-4db1-b4d2-9782dfe2d132}");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("SSV štoparica je že zagnana.");
+            }
         }
     }
 }
